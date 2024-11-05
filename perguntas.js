b3fc7865073193f8a019fae51968520d49348256
Erikito <00001083836274SP@al.educacao.sp.gov.br>
2024-11-05 08:33:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Dom Casmuro',
+    'Quem são os principais personagens da historia?',
+    'Bentinho, Capitu e Escobar'
+)
+
+criaCartao(
+    'Vidas Secas',
+    'Qual tematicas são abordadas na obra?',
+    'Pobresa, Seca, Desigualdade Social e Imigração'
+)
+
+criaCartao(
+    'Capitães da areia',
+    'Aonde a historia é retratada?',
+    'Na cidade de salvador'
+)
+
+criaCartao(
+    'Senhora',
+    'Qual é o desfeicho do romance?',
+    'quando Fernando devolve o dinheiro e está prestes a ir embora, Aurélia declara seu amor por ele. Assim, os dois decidem ficar juntos e, estando apaixonados, acabam vivendo felizes.'
+)
+
+criaCartao(
+    'Iracema',
+    'Quem é o autor da obra?',
+    'José de Alencar'
+)
