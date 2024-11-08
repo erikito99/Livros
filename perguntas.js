3668d52880fa795606dcd65cecd9f73c4cba58e2
Erikito <00001083836274SP@al.educacao.sp.gov.br>
2024-11-08 13:58:50 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,11 @@
 criaCartao(
     'Dom Casmuro',
-    'Quem são os principais personagens da historia?',
-    'Bentinho, Capitu e Escobar'
+    'Conheça um pouco da historia?',
+    'Narrado com foco narrativo em primeira pessoa, seu personagem principal é o carioca de 54 anos Bento de Albuquerque Santiago, advogado solitário e bem-estabelecido que, após ter reproduzido tal qual, no Engenho Novo, a casa em que foi criado "na antiga Rua de Matacavalos" (hoje a Rua do Riachuelo), pretende "atar as duas pontas da vida e resgatar na velhice a adolescência", ou seja, contar na meia idade seus momentos de moço. No primeiro capítulo, o autor justifica o título: é uma homenagem a um "poeta do trem" que certa vez o importunou com seus versos e que lhe chamou de "Dom Casmurro" por ter, segundo Bento, "fechado os olhos três ou quatro vezes" durante a recitação.[7] Seus vizinhos, que lhe estranhavam os "hábitos reclusos e calados", e também seus amigos próximos, popularizaram a alcunha. Para escrever o livro é inspirado por medalhões de César, Augusto, Nero e Massinissa: imperadores romanos que mataram suas esposas adúlteras.[8]
+
+Nos capítulos adiante Bento começa suas reminiscências: conta as experiências que teve quando sua mãe, a viúva D. Glória, lhe enviou para o seminário, fruto de promessa que ela fez caso acabasse concebendo um novo filho depois de seu primeiro, que morreu no parto; a ideia foi ressuscitada pelo agregado José Dias, que conta a Tio Cosme e à D. Glória o namoro de Bentinho com Capitolina, a vizinha pobre por quem Bentinho era apaixonado. No seminário, Bentinho conhece seu melhor amigo, Ezequiel de Sousa Escobar, filho de um advogado de Curitiba. Bento larga o seminário e estuda Direito em São Paulo, enquanto Escobar torna-se comerciante bem-sucedido e casa-se com Sancha, amiga de Capitu. Em 1865, Capitu e Bentinho casam; Sancha e Ezequiel têm uma filha que dão o nome de Capitolina, enquanto o protagonista e sua esposa concebem um filho que chamam de Ezequiel. O Escobar companheiro de Bento, que era exímio nadador, paradoxalmente morre afogado em 1871, e no enterro tanto Sancha quanto Capitu olham o defunto fixamente, "Momento houve em que os olhos de Capitu fitaram o defunto, quais os da viúva, [...], como a vaga do mar lá fora, como se quisesse tragar também o nadador da manhã",[9] segundo ele.
+
+Logo o narrador começa a desconfiar que seu melhor amigo e Capitu o traíam às escondidas. Dom Casmurro também passa a duvidar de sua própria paternidade. Diz ele nas últimas linhas: [...] quis o destino que acabassem juntando-se e enganando-me…[10] O livro termina com o convite irônico "Vamos à História dos Subúrbios",[10] livro que ele, no início do romance, teria pensado escrever antes de ocorrer-lhe a ideia de Dom Casmurro. A ação do romance passa-se entre 1857 e 1875, aproximadamente, e a narrativa, embora de tempo psicológico, permite a percepção de algumas unidades: a infância de Bento em Matacavalos; a casa de Dona Glória e a família do Pádua, com os parentes e agregados; o conhecimento de Capitu; o seminário; a vida conjugal; a densificação do ciúme; os surtos psicóticos de ciúme, agressividade; a ruptura.[11]'
 )
 
 criaCartao(
@@ -20,6 +24,18 @@ criaCartao(
     'Senhora',
     'Qual é o desfeicho do romance?',
     'quando Fernando devolve o dinheiro e está prestes a ir embora, Aurélia declara seu amor por ele. Assim, os dois decidem ficar juntos e, estando apaixonados, acabam vivendo felizes.'
+)
+
+criaCartao(
+    'Iracema',
+    'Quem é o autor da obra?',
+    'José de Alencar'
+)
+
+criaCartao(
+    'Iracema',
+    'Quem é o autor da obra?',
+    'José de Alencar'
 )
 
 criaCartao(
